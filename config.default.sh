5391856f0df9a6041c6c2ad17cd8dabfe01b76d0
timmeinerzhagen <34220101+timmeinerzhagen@users.noreply.github.com>
2020-07-15 13:10:38 +0200

    Adjust ddns and switch to cloudflare

---
+++ config.default.sh
@@ -0,0 +1,16 @@
+mydomain="sos-obx.us" # 'timmeinerzhagen.de'
+myhostname="gateway" # '@'
+gdapikey="api_key:key_secret" # 'xx:xx'
+logdest="local7.info"
+
+# CloudFlare Information
+cf_api_url=""
+cf_email=""
+cf_api_key=""
+zone_id=""
+
+# Update Record Information
+record_id=""
+record_name="@"
+# Logger
+logdest="local7.info"
--- ddns.sh
+++ ddns.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-mydomain="sos-obx.us" # 'timmeinerzhagen.de'
-myhostname="gateway" # '@'
-gdapikey="api_key:key_secret" # 'xx:xx'
-logdest="local7.info"
+source ./config.sh
+
+`${curl_command} -s -X GET -H "Content-Type: application/json"`
 
 myip=`curl -s "https://api.ipify.org"`
-dnsdata=`curl -s -X GET -H "Authorization: sso-key ${gdapikey}" "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${myhostname}"`
+dnsdata=`curl -s -X GET "${cf_api_url}/zones/"#${zone_id}/dns_records?name=${record_name}&type=A"
+    -H "X-Auth-Email: ${cf_email}"
+    -H "X-Auth-Key: ${cf_api_key}"  
+    -H "Content-Type: application/json"`
 gdip=`echo $dnsdata | cut -d ',' -f 1 | tr -d '"' | cut -d ":" -f 2`
 echo "`date '+%Y-%m-%d %H:%M:%S'` - Current External IP is $myip, GoDaddy DNS IP is $gdip"
 
-if [ "$gdip" != "$myip" -a "$myip" != "" ]; then
-  echo "IP has changed!! Updating on GoDaddy"
-  curl -s -X PUT "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${myhostname}" -H "Authorization: sso-key ${gdapikey}" -H "Content-Type: application/json" -d "[{\"data\": \"${myip}\"}]"
-  logger -p $logdest "Changed IP on ${hostname}.${mydomain} from ${gdip} to ${myip}"
-fi
+# if [ "$gdip" != "$myip" -a "$myip" != "" ]; then
+#   echo "IP has changed!! Updating on GoDaddy"
+#   curl -s -X PUT "${cf_api_url}/zones/${zone_id}/dns_records/${record_id}"
+#     -H "X-Auth-Email: ${cf_email}"
+#     -H "X-Auth-Key: ${cf_api_key}" 
+#     -H "Content-Type: application/json" 
+#     -d "{\"id\":\"${zone_id}\",\"type\":\"A\",\"name\":\"${record_name}\",\"content\":\"${record_id}\"}"
+#   logger -p $logdest "Changed IP on from ${gdip} to ${myip}"
+# fi
