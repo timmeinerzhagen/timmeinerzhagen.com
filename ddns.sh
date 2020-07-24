ebbc562ca33cadbd00c25e7da11413f62135597b
Tim Meinerzhagen <timmeinerzhagen@gmail.com>
2020-07-24 04:41:12 +0200

    Adjust ddns script for cloudflare and api tokens

--- ddns.sh
+++ ddns.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-source ./config.sh
+. ./config.conf
 
-`${curl_command} -s -X GET -H "Content-Type: application/json"`
+# Get own current IP
+myip=$(curl -s "https://api.ipify.org")
+# Get currently saved IP from CloudFront
+dnsdata=$(curl -s -X GET "${cf_api_url}/zones/${zone_id}/dns_records?name=${record_name}&type=A" \
+        -H "Authorization: Bearer ${cf_api_key}")
+gdip=$(echo $dnsdata | jq -r ".result[].content")
+echo "`date '+%Y-%m-%d %H:%M:%S'` - Current External IP is $myip, CloudFront IP is $gdip"
 
-myip=`curl -s "https://api.ipify.org"`
-dnsdata=`curl -s -X GET "${cf_api_url}/zones/"#${zone_id}/dns_records?name=${record_name}&type=A"
-    -H "X-Auth-Email: ${cf_email}"
-    -H "X-Auth-Key: ${cf_api_key}"  
-    -H "Content-Type: application/json"`
-gdip=`echo $dnsdata | cut -d ',' -f 1 | tr -d '"' | cut -d ":" -f 2`
-echo "`date '+%Y-%m-%d %H:%M:%S'` - Current External IP is $myip, GoDaddy DNS IP is $gdip"
-
-# if [ "$gdip" != "$myip" -a "$myip" != "" ]; then
-#   echo "IP has changed!! Updating on GoDaddy"
-#   curl -s -X PUT "${cf_api_url}/zones/${zone_id}/dns_records/${record_id}"
-#     -H "X-Auth-Email: ${cf_email}"
-#     -H "X-Auth-Key: ${cf_api_key}" 
-#     -H "Content-Type: application/json" 
-#     -d "{\"id\":\"${zone_id}\",\"type\":\"A\",\"name\":\"${record_name}\",\"content\":\"${record_id}\"}"
-#   logger -p $logdest "Changed IP on from ${gdip} to ${myip}"
-# fi
+if [ "$gdip" != "$myip" -a "$myip" != "" ]; then
+    echo "IP has changed. Updating on CloudFront"
+    res=$( \
+        curl -s -X PUT "${cf_api_url}/zones/${zone_id}/dns_records/${record_id}" \
+            -H "Authorization: Bearer ${cf_api_key}" \
+            -H "Content-Type: application/json" \
+            --data '{"id": "'${zone_id}'", "type":"A","name":"'${record_name}'","content":"'${gdip}'","ttl":1,"proxied":false}')
+    echo "Changed IP in CloudFront from ${gdip} to ${myip}"
+fi
--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@
 sudo apt update
 sudo apt install nginx
 
+# Dependencies
+sudo apt install curl # web calls
+sudo apt install jq # json
+
 # Add DynamicDNS contrab job
 sudo crontab -l > mycron #write out current crontab
 sudo echo "*/5 * * * * sh /home/timclouduser/timmeinerzhagen.de/ddnstest.sh" >> mycron #echo new cron into cron file
