f865458bd3236a0e64d28be943facc9898fc8dba
Tim Jonas Meinerzhagen <timmeinerzhagem@gmail.com>
2020-12-06 17:50:15 +0100

    Add ddns

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 from fastapi import FastAPI
 
+import requests
+import json
+
+
 app = FastAPI()
 
-#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
-
+with open('config/config.json', 'r') as file:
+    config = json.load(file)
 
 @app.get("/")
 def home():
-    return {"message":"Hello TutLinks.com"}
+    return {"message":"Hello World!"}
+
+
+@app.get("/ddns/{new_ip}")
+def dyndns(new_ip):
+    payload =   {
+                    'id':       config["zone_id"],
+                    'type':     'A',
+                    'name':     config["record_name"],
+                    'content':  new_ip,
+                    'ttl':      1,
+                    'proxied':  True
+                }
+
+    headers =   {
+                    'Authorization':    'Bearer ' + config["cf_api_key"],
+                    'Content-Type':     'application/json'
+                }
+
+    response  = requests.put(config["cf_api_url"] + '/zones/' + config["zone_id"] + '/dns_records/' + config["record_id"], 
+            headers = headers, 
+            json = payload)
+
+    return response
