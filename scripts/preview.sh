4d87e556e5219ff81434d77ea04eda9e4da9cf77
Tim Jonas Meinerzhagen <timmeinerzhagem@gmail.com>
2021-04-03 01:40:57 +0200

    Add automation

--- scripts/preview.sh
+++ scripts/preview.sh
@@ -1,5 +1,5 @@
 cd app
 gcloud auth activate-service-account --key-file=../keyfile-dungeonfulldive-com.json
 gcloud config set project dungeonfulldive-com
-gcloud app deploy --no-promote
+gcloud app deploy --no-promote --quiet
 cd ..
